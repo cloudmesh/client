637eab461ad8b085086353825d16522cc6846047
Gregor von Laszewski <laszewski@gmail.com>
2015-06-14 09:01:48 -0400

    added defaults

---
+++ db.py
@@ -0,0 +1,55 @@
+from sqlalchemy import Column, Integer, Unicode, UnicodeText, String
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+from random import choice
+from string import letters
+import sys
+import uuid
+
+debug = False
+
+#engine = create_engine('sqlite:////tmp/test.db', echo=debug)
+engine = create_engine('sqlite://')
+Base = declarative_base(bind=engine)
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    address = Column(String, nullable=True)
+    password = Column(String)
+    uuid = Column(String)
+
+    def __init__(self, name, address=None, password=None):
+        self.name = name
+        self.address = address
+        if password is None:
+            password = ''.join(choice(letters) for n in xrange(10))
+        self.password = password
+        self.uuid = str(uuid.uuid4())
+
+Base.metadata.create_all()
+
+Session = sessionmaker(bind=engine)
+s = Session()
+
+
+
+# create instances of my user object
+u = User('gregor')
+u.address = '101 somewhere'
+
+u2 = User(u'albert')
+u2.password = 'hdjkhdljkvhl'
+
+# testing
+s.add_all([u, u2])
+
+s.commit()
+
+# When you query the data back it returns instances of your class:
+
+for user in s.query(User):
+    print user.name, user.password, user.uuid, user.id
---
+++ vm.py
@@ -0,0 +1,138 @@
+from sqlalchemy import Column, Integer, Unicode, UnicodeText, String
+from sqlalchemy import create_engine
+from sqlalchemy import update
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from prettytable import PrettyTable
+from cloudmesh_base.tables import dict_printer
+import uuid
+import os
+import getpass
+import json
+from pprint import pprint
+
+debug = False
+
+#engine = create_engine('sqlite:////tmp/test.db', echo=debug)
+engine = create_engine('sqlite://')
+Base = declarative_base(bind=engine)
+
+class DEFAULT(Base):
+    __tablename__ = 'default'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    value = Column(String)
+
+
+class VM(Base):
+    __tablename__ = 'vm'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    label = Column(String)
+    group = Column(String)
+    uuid = Column(String)
+    cloud = Column(String)
+    user = Column(String)
+
+    def __init__(self,
+                 name,
+                 label=None,
+                 group=None,
+                 cloud=None,
+                 user=None):
+
+        self.name = name
+        self.label = label
+
+        if label is None:
+            self.label = name
+        if group is None:
+            self.group = 'default'
+        if user is None:
+            self.user = getpass.getuser()
+
+        self.uuid = str(uuid.uuid4())
+
+class DB (object):
+
+    def add(self, items):
+        self.session.add_all(items)
+
+    def __init__(self):
+        Base.metadata.create_all()
+        self.session = self.connect()
+
+    def connect(self):
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
+        return self.session
+
+    def save(self):
+        self.session.commit()
+
+    def default(self, key, value):
+        # find
+        d = [DEFAULT(name=key, value=value)]
+        current = self.session.query(DEFAULT).filter_by(name=key).first()
+        if current is None:
+            self.session.add_all(d)
+        else:
+            current.value = value
+            self.save()
+        self.save()
+
+    @property
+    def data(self):
+        return self.session
+
+    def dict(self, table):
+        result = dict()
+        for u in self.session.query(table).all():
+            _id = u.id
+            values = {}
+            for key in u.__dict__.keys():
+                if not key.startswith("_sa"):
+                    values[key] = u.__dict__[key]
+            result[_id] = values
+        return result
+
+    def json(self, table):
+        d = self.dict(table)
+        return json.dumps(d)
+
+db = DB()
+
+# create instances of my user object
+vm = []
+vm.append(VM('gregor1'))
+vm.append(VM('gregor2'))
+
+db.add(vm)
+db.save()
+
+# When you query the data back it returns instances of your class:
+
+for v in db.data.query(VM):
+    print v.name, v.label, v.uuid, v.id
+
+pprint (db.dict(VM))
+pprint (db.json(VM))
+
+
+print (dict_printer(db.dict(VM)))
+
+
+d = [DEFAULT(name="user", value="albert")]
+db.add(d)
+db.save()
+
+db.default("hallo", "gregor")
+db.default("user", "xyz")
+
+for v in db.data.query(DEFAULT):
+    print v.name, v.value
+
+print (dict_printer(db.dict(DEFAULT)))
+
+#                    order=['name', 'value'],
+#                    header=['Name', 'Value']))
