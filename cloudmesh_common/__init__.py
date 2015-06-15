1f1ba5a036e00cfe6bb66031d42418575b0a942b
Gregor von Laszewski <laszewski@gmail.com>
2015-06-15 09:23:18 -0400

    testing new version of pbr

--- cloudmesh_common/__init__.py
+++ cloudmesh_common/__init__.py
@@ -1,1 +1,6 @@
+import pbr.version
+
+__version__ = pbr.version.VersionInfo('python-novaclient').version_string()
+
+
 version = "0.1"
---
+++ deprecated/setup-ok.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+version = "0.1"
+
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import glob
+import os
+
+package_name = "cloudmesh_common"
+
+try:
+    from cloudmesh_base.util import banner
+except:
+    os.system("pip install cloudmesh_base")
+
+from cloudmesh_base.util import banner
+from cloudmesh_base.util import path_expand
+from cloudmesh_base.util import auto_create_version
+
+banner("Installing Cloudmesh " + package_name)
+
+home = os.path.expanduser("~")
+
+auto_create_version(package_name, version)
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+requirements = parse_requirements('requirements.txt')
+
+
+
+class UploadToPypi(install):
+    """Upload the package to pypi."""
+    def run(self):
+        os.system("Make clean Install")
+        os.system("python setup.py.in install")
+        banner("Build Distribution")
+        os.system("python setup.py.in sdist --format=bztar,zip upload")
+
+
+class RegisterWithPypi(install):
+    """Upload the package to pypi."""
+    def run(self):
+        banner("Register with Pypi")
+        os.system("python setup.py.in register")
+
+
+class InstallBase(install):
+    """Install the package."""
+    def run(self):
+        banner("Installing Cloudmesh " + package_name)
+        install.run(self)
+
+
+class InstallRequirements(install):
+    """Install the requirements."""
+    def run(self):
+        banner("Installing Requirements for Cloudmesh " + package_name)
+        os.system("pip install -r requirements.txt")
+
+
+class InstallAll(install):
+    """Install requirements and the package."""
+    def run(self):
+        banner("Installing Requirements for Cloudmesh " + package_name)
+        os.system("pip install -r requirements.txt")
+        banner("Installing Cloudmesh " + package_name)
+        install.run(self)
+
+setup(
+    name='cloudmesh_common',
+    version=version,
+    description='A set of simple base functions and classes useful for cloudmesh and other programs',
+    # description-file =
+    #    README.rst
+    author='The Cloudmesh Team',
+    author_email='laszewski@gmail.com',
+    url='http://github.org/cloudmesh/base',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering',
+        'Topic :: System :: Clustering',
+        'Topic :: System :: Distributed Computing',
+        'Topic :: System :: Boot',
+        'Topic :: System :: Systems Administration',
+        'Framework :: Flask',
+        'Environment :: OpenStack',
+    ],
+    packages=find_packages(),
+    install_requires=requirements,
+    cmdclass={
+        'install': InstallBase,
+        'requirements': InstallRequirements,
+        'all': InstallAll,
+        'pypi': UploadToPypi,
+        'pypiregister': RegisterWithPypi,
+        },
+)
+
