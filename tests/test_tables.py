fb92b7109e3d700f59b96ac4129fc8680603930f
Gregor von Laszewski <laszewski@gmail.com>
2015-06-17 09:30:29 -0400

    adding the test method templates for tables

--- tests/test_tables.py
+++ tests/test_tables.py
@@ -13,7 +13,20 @@ from cloudmesh_base.util import HEADING
 
 class Test_tables:
 
+    """
+    define tests for dict printer so you test
+
+    yaml
+    json
+    table
+    csv
+    dict
+
+    printing
+
+    """
     def setup(self):
+        self.d = {""} # put your dict here, may be generated than you can use as self.d
         pass
 
     def tearDown(self):
@@ -22,3 +35,25 @@ class Test_tables:
     def test_dummy(self):
         HEADING()
         assert True
+
+    def test_001_yaml(self):
+        HEADING()
+        assert False
+
+    def test_002_json(self):
+        HEADING()
+        assert False
+
+    def test_003_table(self):
+        HEADING()
+        assert False
+
+    def test_004_dict(self):
+        HEADING()
+        assert False
+
+    def test_005_csv(self):
+        HEADING()
+        assert False
+
+
