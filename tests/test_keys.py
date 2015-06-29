3c0890e5d07cf526b0930bef3490c0bca48e15a8
Paulo Chagas <paulo.robertojr100@gmail.com>
2015-06-29 10:48:33 -0400

    updating test_keys

---
+++ tests/test_keys.py
@@ -0,0 +1,53 @@
+""" run with
+
+nosetests -v --nocapture
+
+or
+
+nosetests -v
+
+"""
+
+from cloudmesh_base.util import HEADING
+import os
+from cloudmesh_base.util import banner
+from cloudmesh_client.keys.Keys import SSHkeys
+
+
+class Test_keys:
+    def setup(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_001(self):
+        """testing cm search with all the arguments"""
+        HEADING()
+        try:
+
+
+            mykeys = SSHkeys()
+
+            banner("ssh keys")
+            pprint (SSHkeys.find_in_dir("~/.ssh"))
+
+            banner("git hub")
+            pprint (SSHkeys.get_key_from_git('laszewsk'))
+
+            banner("all")
+            pprint (SSHkeys.get_all_keys('laszewsk'))
+            assert False
+        except Exception:
+            assert True
+
+    def tes_002(self):
+        from pprint import pprint
+
+        sshkey = SSHkey("~/.ssh/id_rsa.pub")
+        pprint (sshkey.key)
+        print ("Fingerprint:", sshkey.fingerprint)
+        pprint (sshkey.__key__)
+        print ("sshkey", sshkey)
+        print ("str", str(sshkey))
+        print (sshkey.type)
