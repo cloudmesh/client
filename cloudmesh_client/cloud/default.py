fc6fb8ec21f595d1151bc76033c7a1a9544cb89d
Gregor von Laszewski <laszewski@gmail.com>
2015-08-04 09:50:57 -0400

    adding tests for default and Default.clear images flavor group

--- cloudmesh_client/cloud/default.py
+++ cloudmesh_client/cloud/default.py
@@ -18,6 +18,10 @@ class Default(object):
                                       'name',
                                       'value'], output=format))
 
+    #
+    # GENERAL SETER AND GETER METHOD
+    #
+
     @classmethod
     def set(cls, key, value, cloud):
         cm = CloudmeshDatabase()
@@ -30,18 +34,6 @@ class Default(object):
         return(cm.get_default(key, cloud))
 
     @classmethod
-    def cloud(cls, key):
-        cm = CloudmeshDatabase()
-        return(cm.get_default(key, "general"))
-
-    @classmethod
-    def set_cloud(cls, key, value):
-        cm = CloudmeshDatabase()
-        d = cm.dict(DEFAULT)
-        cm.set_default(key, value, "general")
-
-
-    @classmethod
     def delete(cls, key, cloud):
         cm = CloudmeshDatabase()
         _id = cm.getID("default", key, cloud)
@@ -49,5 +41,71 @@ class Default(object):
         if e is not None:
             cm.delete(e)
 
+    @classmethod
+    def clear(cls):
+        cm = CloudmeshDatabase()
+        d = cm.all(DEFAULT)
+        for item in d:
+            name = d[item]["name"]
+            kind = DEFAULT
+            cm.delete_by_name(kind, name)
+
+    #
+    # Set the default cloud
+    #
+    @classmethod
+    def get_cloud(cls):
+        return(cls.get("cloud", "general"))
+
+    @classmethod
+    def set_cloud(cls, value):
+        cls.set("cloud", value, "general")
 
 
+    #
+    # Set the default image
+    #
+
+    @classmethod
+    def set_image(cls, value, cloud):
+        cls.set("image", value, cloud)
+
+    @classmethod
+    def get_image(cls, cloud):
+        return(cls.get("image", cloud))
+
+    #
+    # Set the default flavor
+    #
+
+    @classmethod
+    def set_flavor(cls, value, cloud):
+        cls.set("flavor", value, cloud)
+
+    @classmethod
+    def get_flavor(cls, cloud):
+        return(cls.get("flavor", cloud))
+
+    #
+    # Set the default group
+    #
+
+    @classmethod
+    def set_group(cls, value):
+        cls.set("group", value, "general")
+
+    @classmethod
+    def get_group(cls):
+        return(cls.get("group", "general"))
+
+    #
+    # Set the default key
+    #
+
+    @classmethod
+    def set_key(cls, value):
+        cls.set("key", value, "general")
+
+    @classmethod
+    def get_key(cls):
+        return(cls.get("key", "general"))
---
+++ tests/test_default.py
@@ -0,0 +1,145 @@
+""" run with
+
+python setup.py install; nosetests -v --nocapture  tests/test_default.py:Test_default.test_001
+
+nosetests -v --nocapture tests/test_default.py
+
+or
+
+nosetests -v tests/test_vm.py
+
+"""
+from cloudmesh_base.Shell import Shell
+from cloudmesh_base.util import HEADING
+import os
+from time import sleep
+from cloudmesh_client.cloud.default import Default
+
+def run(command):
+    parameter = command.split(" ")
+    shell_command = parameter[0]
+    args = parameter[1:]
+    result = Shell.execute(shell_command, args)
+    return result
+
+class Test_default():
+    """
+
+    """
+
+
+    def setup(self):
+        pass
+
+
+
+
+    def test_001(self):
+        """
+        delete defaults
+        :return:
+        """
+        HEADING()
+
+        Default.clear()
+        print(Default.list())
+        assert True
+
+    def _check(self, content):
+        result = Default.list()
+        print(result)
+        assert content in str(result)
+
+
+    def test_002(self):
+        """
+        set default cloud
+        :return:
+        """
+        HEADING()
+
+        name = "mycloud"
+        Default.set_cloud(name)
+        assert Default.get_cloud() == name
+
+        self._check(name)
+
+    def test_003(self):
+        """
+        set default image
+        :return:
+        """
+        HEADING()
+
+        name = "myimage"
+        Default.set_image(name,"mycloud")
+        assert Default.get_image("mycloud") == name
+
+        self._check(name)
+
+    def test_004(self):
+        """
+        set default flavor
+        :return:
+        """
+        HEADING()
+
+        name = "myflavor"
+        Default.set_flavor(name,"mycloud")
+        assert Default.get_flavor("mycloud") == name
+
+        self._check(name)
+
+    def test_005(self):
+        """
+        set default key
+        :return:
+        """
+        HEADING()
+
+        name = "mykey"
+        Default.set_key(name)
+        assert Default.get_key() == name
+
+        self._check(name)
+
+    def test_006(self):
+        """
+        set default key
+        :return:
+        """
+        HEADING()
+
+        name = "mygroup"
+        Default.set_group(name)
+        assert Default.get_group() == name
+
+        self._check(name)
+
+    def test_007(self):
+        """
+        set default variable
+        :return:
+        """
+        HEADING()
+
+        name = "myvar"
+        value = "myvalue"
+        cloud = "mycloud"
+        Default.set(name, value, cloud)
+        assert Default.get(name, cloud) == value
+
+        self._check(value)
+
+
+    '''
+    def test_002(self):
+        """
+        tries to start a vm with an invalid image
+        :return:
+        """
+        HEADING()
+        result = run ("cm vm start --cloud=india --flavor=m1.medium --image=futuresystems/linux>windows")
+
+        assert "not found" in result
+    '''
