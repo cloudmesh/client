84355d01b342bcfe9d135b0b1b30b644523f4be0
Gregor von Laszewski <laszewski@gmail.com>
2015-08-05 20:16:01 -0400

    simplified openstack interface

--- test.py
+++ test.py
@@ -1,21 +1,45 @@
 import os
 from novaclient.client import Client
 from cloudmesh_base.util import path_expand
+from cloudmesh_base.ConfigDict import ConfigDict
 import sys
 import json
+from pprint import pprint
+from cloudmesh_base.MultiKeyDict import MultiKeyDict
+
+# disable security warning for old certs
+import requests.packages.urllib3
+requests.packages.urllib3.disable_warnings()
 
 
-def get_nova_credentials_v2():
-     d = {}
-     d['version'] = '2'
-     d['username'] = os.environ['OS_USERNAME']
-     d['api_key'] = os.environ['OS_PASSWORD']
-     d['auth_url'] = os.environ['OS_AUTH_URL']
-     d['project_id'] = os.environ['OS_TENANT_NAME']
-     d['cacert'] = path_expand(os.environ['OS_CACERT'])
-     return d
 
+def get_nova_credentials(kind="yaml", cloud=None):
 
+    d = {}
+    if kind in ["env"]:
+        d['version'] = '2'
+        d['username'] = os.environ['OS_USERNAME']
+        d['api_key'] = os.environ['OS_PASSWORD']
+        d['auth_url'] = os.environ['OS_AUTH_URL']
+        d['project_id'] = os.environ['OS_TENANT_NAME']
+        d['cacert'] = path_expand(os.environ['OS_CACERT'])
+    elif kind in ["yaml"]:
+        if cloud is None:
+            raise Exception("cloud not specified")
+        config = dict(ConfigDict(filename="~/.cloudmesh/cloudmesh.yaml")["cloudmesh"]["clouds"][cloud])
+        cred = dict(config["credentials"])
+        d['version'] = '2'
+        d['username'] = cred['OS_USERNAME']
+        d['api_key'] = cred['OS_PASSWORD']
+        d['auth_url'] = cred['OS_AUTH_URL']
+        d['project_id'] = cred['OS_TENANT_NAME']
+        if 'OS_CACERT' in cred:
+            d['cacert'] = path_expand(cred['OS_CACERT'])
+    else:
+        raise Exceotion ("unsupported kind: " + kind)
+    return d
+
+'''
 openstack_client_keys  = [
     ["username", "OS_USERNAME"],
     ["password", "OS_PASSWORD",  "api_key"],
@@ -24,42 +48,12 @@ openstack_client_keys  = [
     ["region_name", "OS_REGION_NAME"],
     ["cacert", "OS_CACERT"],
     ["version", "OS_COMPUTE_API_VERSION"],
-    ["project_id", "OS_TENANT_NAME", ],
+    ["project_id", "OS_TENANT_NAME"],
+    ["os_version", "OS_VERSION"]
 ]
 
-class MultiKeyDict(object):
-    
-    def __init__(self, m):
-        self.element = {
-            "keys" : m,
-            "value" : {}
-            }
-           
-    def dump(self):
-        print(json.dumps(self.element, indent=4))
-
-
-    def set(self, d):
-        for key in d:
-           self.__setitem__(key, d[key])
-
-    def _getkey(self, key):
-        for k in self.element["keys"]:
-            if key in k:
-                return k[0]
-        raise Exception("key not found: " + key)
-
-    def __getitem__(self, key):
-        return self.element["value"][self._getkey(key)] 
-
-    def __setitem__(self, key, value):
-        self.element["value"][self._getkey(key)] = value
-    
-    def __str__(self):
-        return str(dict(self.element["value"]))
 
 m = MultiKeyDict(openstack_client_keys)
-
 m["username"] = "gregor"
 print ("U", m["username"])
 print ("U", m["OS_USERNAME"])
@@ -68,16 +62,91 @@ m["OS_PASSWORD"] = "1234"
 print("P", m["password"])
 print("P", m["OS_PASSWORD"])
 
+# m.set(get_nova_credentials_v2())
+'''
 
 
-m.set(get_nova_credentials_v2())
+class CloudMesh(object):
 
-m.dump()
+    def __init__(self):
+        self.config = dict(ConfigDict(filename="~/.cloudmesh/cloudmesh.yaml")["cloudmesh"]["clouds"])
+        self.client = {}
 
-sys.exit()
+    def cloud_type(self, cloud):
+        return self.config[cloud]["cm_type"]
+
+    def credentials(self, cloud):
+        if self.cloud_type(cloud) == "openstack":
+            cred = get_nova_credentials(cloud=cloud)
+            return dict(cred)
+        else:
+            print("not supported cloud type")
+
+    def authenticate(self, cloud):
+        credential = self.credentials(cloud)
+        self.client[cloud] = Client(**credential)
+
+    def list(self, kind, cloud=None):
+        if kind in ["server"]:
+            objects = self.client[cloud].servers.list()
+        elif kind in ["flavor"]:
+            objects = self.client[cloud].flavors.list()
+        elif kind in ["image"]:
+            objects = self.client[cloud].images.list()
+        else:
+            raise Exception("unsupported kind or cloud: " + kind + " " + str(cloud))
+        result = {cloud: {}}
+        for i in objects:
+            result[cloud][i.name] = i.__dict__
+        return result
+
+#mesh = Mesh()
+
+#cred = mesh.credentials("india")
+#print("CCC", cred)
 
 
-print (m)
+credential = get_nova_credentials(cloud="india")
+pprint (credential)
+
+# credential = get_nova_credentials(kind="env")
+# pprint (credential)
+
+
+nova_client = Client(**credential)
+
+print(nova_client.servers.list())
+
+credential = get_nova_credentials(cloud="chameleon")
+
+pprint (credential)
+
+# credential = get_nova_credentials(kind="env")
+# pprint (credential)
+
+
+nova_client = Client(**credential)
+
+print(nova_client.servers.list())
+
+print("===================")
+cm = CloudMesh()
+
+cm.authenticate("india")
+cm.authenticate("chameleon")
+
+l = cm.list("server", cloud="india")
+pprint (l)
+l = cm.list("server", cloud="chameleon")
+pprint (l)
+l = cm.list("flavor", cloud="india")
+pprint (l)
+l = cm.list("image", cloud="india")
+pprint (l)
+l = cm.list("flavor", cloud="chameleon")
+pprint (l)
+l = cm.list("image", cloud="chameleon")
+pprint (l)
 
 '''
 export OS_USERNAME=username
@@ -120,8 +189,3 @@ auth (str) - Auth
 '''
 
 
-credentials = get_nova_credentials_v2()
-
-nova_client = Client(**credentials)
-
-print(nova_client.servers.list())
