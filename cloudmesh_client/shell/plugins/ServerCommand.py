60c69f0f8009b3c0638e2d8b5cdf69c6c10357fb
Gregor von Laszewski <laszewski@gmail.com>
2015-09-03 20:48:02 -0400

    sandman integration

---
+++ cloudmesh_client/shell/plugins/ServerCommand.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python
+
+from docopt import docopt
+from sandman import app
+from cloudmesh_client.shell.console import Console
+from cloudmesh_client.shell.command import command
+from cloudmesh_base.util import path_expand
+
+
+class ServerCommand(object):
+
+    topics = {"default": "cloud"}
+
+    def __init__(self, context):
+        self.context = context
+        if self.context.debug:
+            print("init command default")
+
+    @command
+    def do_server(self, args, arguments):
+        """
+        Usage:
+            server
+
+        Options:
+          -h --help
+          -v       verbose mode
+
+        Description:
+          Starts up a REST service and a WEB GUI so one can browse the data in an
+          existing cloudmesh database.
+
+          The location of the database is supposed to be in
+
+            ~/.cloud,esh/cloudmesh.db
+
+        """
+
+        filename = "sqlite:///{}".format(path_expand("~/.cloudmesh/cloudmesh.db"))
+
+        print("database: {}".format(filename))
+        app.config['SQLALCHEMY_DATABASE_URI'] = filename
+
+        from sandman.model import activate
+
+        activate()
+
+        app.run()
+
+if __name__ == "__main__":
+    main()
