c4fcaf8604da1d958e1665760680f5804257cca4
Gregor von Laszewski <laszewski@gmail.com>
2015-09-07 20:43:47 -0400

    :fix: usr: improving cloud command documentation

--- cloudmesh_client/shell/plugins/CloudCommand.py
+++ cloudmesh_client/shell/plugins/CloudCommand.py
@@ -20,20 +20,51 @@ class CloudCommand(object):
 
           Usage:
               cloud list [--output=FORMAT]
-
+              cloud activate CLOUD
+              cloud deactivate CLOUD
+              cloud info CLOUD
 
           managing the admins test test test test
 
           Arguments:
-
             KEY    the name of the admin
             VALUE  the value to set the key to
 
           Options:
-
              --cloud=CLOUD    the name of the cloud [cloud: general]
              --output=FORMAT  the output format [cloud: table]
 
+          Description:
+             Cloudmesh contains a cloudmesh.yaml file that containes templates for multiple clouds that you may or
+             may not have access to. Hence it is useful to activate and deacivate clouds you like to use in other
+             commands.
+
+             To activate a cloud a user can simply use the activate command followed by the name of the cloud to be
+             activated. To find out which clouds are available you can use the list command that will provide you
+             with some basic information. As default it will print a table. Thus the commands
+
+
+               cloud activate india
+               cloud deactivate aws
+
+             Will result in
+
+                +----------------------+--------+-------------------+
+                | Cloud name           | Active | Type              |
+                +----------------------+--------+-------------------+
+                | india                | True   | Openstack         |
+                +----------------------+--------+-------------------+
+                | aws                  | False  | AWS               |
+                +----------------------+--------+-------------------+
+
+             To get ore information about the cloud you can use the command
+
+                cloud info CLOUD
+
+             It will call internally also the command uses in regitser
+
+          See also:
+             register
         """
         # pprint(arguments)
         cloud = arguments["--cloud"]
