8a257dcb5ba55a0b9abba0b9a9fd9f6ca1611d50
Gregor von Laszewski <laszewski@gmail.com>
2015-10-27 09:07:23 -0400

    chg: dev: adding a new proposed ListResource Abstract base class

---
+++ cloudmesh_client/cloud/ListResource.py
@@ -0,0 +1,90 @@
+from abc import ABCMeta, abstractmethod
+
+class ListResource(metaclass=ABCMeta):
+
+
+    @abstractmethod
+    @classmethod
+    def info(cls, **kwargs):
+        raise NotImplementedError()
+
+    @abstractmethod
+    @classmethod
+    def list(cls, **kwargs):
+        raise NotImplementedError()
+
+
+    @abstractmethod
+    @classmethod
+    def clear(cls, **kwargs):
+        raise NotImplementedError()
+
+    @abstractmethod
+    @classmethod
+    def refresh(cls, **kwargs):
+        raise NotImplementedError()
+
+    @abstractmethod
+    @classmethod
+    def get(cls, **kwargs):
+        raise NotImplementedError()
+
+    @abstractmethod
+    @classmethod
+    def copy(cls, **kwargs):
+        raise NotImplementedError()
+
+    @abstractmethod
+    @classmethod
+    def merge(cls, **kwargs):
+        raise NotImplementedError()
+
+    @abstractmethod
+    @classmethod
+    def __delitem__(cls, a, b):
+        raise NotImplementedError()
+
+    @abstractmethod
+    @classmethod
+    def __add__(cls, a, b):
+        raise NotImplementedError()
+
+    @abstractmethod
+    @classmethod
+    def __iadd__(cls, a, b):
+        raise NotImplementedError()
+
+    @abstractmethod
+    @classmethod
+    def __isub__(cls, a, b):
+        raise NotImplementedError()
+
+    @abstractmethod
+    @classmethod
+    def __concat__(cls, a, b):
+        raise NotImplementedError()
+
+    @abstractmethod
+    @classmethod
+    def __contains__(cls, a, b):
+        raise NotImplementedError()
+
+    @abstractmethod
+    @classmethod
+    def __sub__(cls, a, b):
+        raise NotImplementedError()
+
+    @abstractmethod
+    @classmethod
+    def __getitem__(cls, item):
+        raise NotImplementedError()
+
+    @abstractmethod
+    @classmethod
+    def __setitem__(cls, item):
+        raise NotImplementedError()
+
+    @abstractmethod
+    @classmethod
+    def count(self):
+        raise NotImplementedError()
