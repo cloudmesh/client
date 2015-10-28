7d65e444003877aa90f6d032ac383363735e36c0
fugangwang <kevinwangfg@gmail.com>
2015-10-28 15:03:57 -0400

    example of working with keystone v3

---
+++ examples/nova_ksv3.py
@@ -0,0 +1,30 @@
+from keystoneclient.auth.identity import v3
+from keystoneclient import session
+from novaclient import client
+import os
+
+# credentials from sys env or configuration
+AUTH_URL = os.getenv("OS_AUTH_URL")
+USERNAME = os.getenv("OS_USERNAME")
+PASSWORD = os.getenv("OS_PASSWORD")
+PROJECT_NAME = os.getenv("OS_PROJECT_NAME")
+USER_DOMAIN = os.getenv("OS_USER_DOMAIN_ID")
+PROJECT_DOMAIN = os.getenv("OS_PROJECT_DOMAIN_ID")
+CERT = os.getenv("OS_CACERT")
+
+# auth to identity v3
+ksauth = v3.Password(auth_url=AUTH_URL,
+                   username=USERNAME,
+                   password=PASSWORD,
+                   project_name=PROJECT_NAME,
+                   user_domain_name=USER_DOMAIN,
+                   project_domain_name=PROJECT_DOMAIN)
+
+# nova client v2 based on previous ks auth
+sess = session.Session(auth=ksauth, verify=CERT)
+nova = client.Client(2, session=sess)
+
+print nova.servers.list()
+print nova.flavors.list()
+print nova.images.list()
+
