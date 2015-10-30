67553db33c7605deecd3bb13b2e303886a21695e
Gourav Shenoy <shenoy.200@gmail.com>
2015-10-30 00:16:51 -0400

    chg: dev: Skeleton for network implementation

---
+++ cloudmesh_client/cloud/network.py
@@ -0,0 +1,40 @@
+from __future__ import print_function
+
+
+class Network(object):
+
+    @classmethod
+    def fixed_ip_get(cls, cloud, fixed_ip):
+        pass
+
+    @classmethod
+    def fixed_ip_reserve(cls, cloud, fixed_ip):
+        pass
+
+    @classmethod
+    def fixed_ip_unreserve(cls, cloud, fixed_ip):
+        pass
+
+    @classmethod
+    def floating_ip_associate(cls, cloud, server, floating_ip):
+        pass
+
+    @classmethod
+    def floating_ip_disassociate(cls, cloud, server, floating_ip):
+        pass
+
+    @classmethod
+    def floating_ip_create(cls, cloud, floating_pool=None):
+        pass
+
+    @classmethod
+    def floating_ip_delete(cls, cloud, floating_ip):
+        pass
+
+    @classmethod
+    def floating_ip_list(cls, cloud):
+        pass
+
+    @classmethod
+    def floating_ip_pool_list(cls, cloud):
+        pass
