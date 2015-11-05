be5974375442283104dfca24452c2c59114e3c21
Gregor von Laszewski <laszewski@gmail.com>
2015-11-05 15:41:42 -0500

    chg: dev: change cls to self

--- cloudmesh_client/cloud/iaas/CloudProviderOpenstackAPI.py
+++ cloudmesh_client/cloud/iaas/CloudProviderOpenstackAPI.py
@@ -223,7 +223,8 @@ class CloudProviderOpenstackAPI(CloudProviderBase):
     def list_limits(self, cloud, output="table", tenant=None):
         try:
             # nova = CloudProvider.setet((cloud)
-            result = cls.nova.limits.get(tenant_id=tenant)._info["absolute"]
+            result = self.provider.limits.get(tenant_id=tenant)._info[
+                "absolute"]
             return attribute_printer(result, output=output)
         except Exception, e:
             return e
@@ -285,59 +286,3 @@ class CloudProviderOpenstackAPI(CloudProviderBase):
             ]
         return (order, header)
 
-    """
-    def list(self, cloud, start, end, tenant, format):
-        # TODO: consider named arguments
-        #def list(self, cloud, start=None,
-        #         end=None, tenant=None, output="table"):
-        # set the environment variables
-        set_os_environ(cloud)
-        try:
-            # execute the command
-            args = ["usage"]
-            if start is not None:
-                args.extend(["--start", start])
-            if end is not None:
-                args.extend(["--end", end])
-            if tenant is not None:
-                args.extend(["--tenant", tenant])
-
-            result = Shell.execute("nova", args)
-            result = Nova.remove_subjectAltName_warning(result)
-
-            lines = result.splitlines()
-            dates = lines[0]
-
-            table = '\n'.join(lines[1:])
-
-            dates = dates.replace("Usage from ", "").\
-                replace("to", "").replace(" +", " ")[:-1].split()
-
-            #
-            # TODO: for some reason the nova command has returned not the
-            # first + char, so we could not ignore the line we may set - as
-            # additional comment char, but that did not work
-            #
-            d = TableParser.convert(result, comment_chars="+#")
-
-
-            d["0"]["start"] = "start"
-            d["0"]["end"] = "end"
-
-            d["1"]["start"] = dates[0]
-            d["1"]["end"] = dates[1]
-
-            del d['0']
-
-            return dict_printer(d,
-                                       order=["start",
-                                              "end",
-                                              "servers",
-                                              "cpu hours",
-                                              "ram mb-hours",
-                                              "disk gb-hours"],
-                                       output=format)
-
-        except Exception, e:
-            return e
-    """
