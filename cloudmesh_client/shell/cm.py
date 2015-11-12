6a70fabb58ed91d532ad6b234782b4b5554105a1
Gregor von Laszewski <laszewski@gmail.com>
2015-11-12 07:38:50 -0500

    new: usr: allowing commenst in cmd with #, // and /*, they must be on one line and at the beginning

--- cloudmesh_client/shell/cm.py
+++ cloudmesh_client/shell/cm.py
@@ -62,6 +62,38 @@ class CloudmeshConsole(cmd.Cmd, PluginCommandClasses):
     # class CloudmeshConsole(cmd.Cmd,
     #                       ConsoleClasses(PluginCommand)):
 
+    def onecmd(self, line):
+        """Interpret the argument as though it had been typed in response
+        to the prompt.
+
+        This may be overridden, but should not normally need to be;
+        see the precmd() and postcmd() methods for useful execution hooks.
+        The return value is a flag indicating whether interpretation of
+        commands by the interpreter should stop.
+
+        """
+        cmd, arg, line = self.parseline(line)
+        if not line:
+            return self.emptyline()
+        if line.startswith('#') \
+                or line.startswith('//') \
+                or line.startswith('/*'):
+            print (line)
+            return self.emptyline()
+        if cmd is None:
+            return self.default(line)
+        self.lastcmd = line
+        if line == 'EOF' :
+            self.lastcmd = ''
+        if cmd == '':
+            return self.default(line)
+        else:
+            try:
+                func = getattr(self, 'do_' + cmd)
+            except AttributeError:
+                return self.default(line)
+            return func(arg)
+
     def register_topics(self):
         topics = {}
         for command in PluginCommand.__subclasses__():
