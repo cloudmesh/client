6a9c1cc10dfff58e137226c71a24cd4c2f7efec3
Gregor von Laszewski <laszewski@gmail.com>
2016-02-15 12:09:33 -0500

    chg:dev: improve script testing

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -15,13 +15,6 @@ from cloudmesh_client.util import HEADING
 from cloudmesh_client.common.Shell import Shell
 from cloudmesh_client.util import banner
 
-def run(command):
-    banner(command)
-    parameter = command.split(" ")
-    shell_command = parameter[0]
-    args = parameter[1:]
-    result = Shell.execute(shell_command, args)
-    return result
 
 
 class Test_script():
@@ -32,16 +25,26 @@ class Test_script():
         "group": "mygroup"
     }
 
+    def run(self, command):
+        command = command.format(**self.data)
+        banner(command)
+        parameter = command.split(" ")
+        shell_command = parameter[0]
+        args = parameter[1:]
+        result = Shell.execute(shell_command, args)
+        print (result)
+        return result
+
     def setup(self):
         self.scripts = [
-            #("bash.cm", "README.rst"),
-            #("comment.cm", "/* test"),
-            #("var.cm", "time ="),
-            #("py.cm", "2"),
-            #("terminal.cm", ""),
+            ("bash.cm", "README.rst"),
+            ("comment.cm", "/* test"),
+            ("var.cm", "username"),
+            ("py.cm", "2"),
+            ("terminal.cm", ""),
             #("hpc.cm", "bravo"),
-            #("key.cm", ""),
-            #("reservedemo.cm", "cloudnauts"),
+            ("key.cm", ""),
+            ("reservedemo.cm", "cloudnauts"),
             #("cloud.cm", "Active"),
             #("nova.cm", "Status"),
             #("demo.cm", "ephemeral_disk"),
@@ -64,12 +67,10 @@ class Test_script():
         """
 
         HEADING()
-        cloud = "india"
-        data = {}
-        for data["script"], data["check"] in self.scripts:
-            command = "cm scripts/{script}".format(**data)
-            result = run(command)
+        for self.data["script"], self.data["check"] in self.scripts:
+            command = "cm scripts/{script}"
+            result = self.run(command)
             print (result)
-            assert data["check"] in result
+            assert self.data["check"] in result
 
 
