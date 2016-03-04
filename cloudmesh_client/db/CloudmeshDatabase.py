cf8e5aa5a59d033478b8adc702e959c32964a61b
Thanmai Bindi <thanmai.m17@gmail.com>
2016-03-04 14:39:43 -0500

    fix:hackathon: changed cloudmeshDatabase to have the changes of list for values.

--- cloudmesh_client/db/CloudmeshDatabase.py
+++ cloudmesh_client/db/CloudmeshDatabase.py
@@ -134,7 +134,7 @@ class CloudmeshDatabase(object):
 
                 if kind == "flavor":
                     flavors = provider.list_flavor(name)
-                    for flavor in flavors.values():
+                    for flavor in list(flavors.values()):
                         flavor["uuid"] = flavor['id']
                         flavor['type'] = 'string'
                         flavor["category"] = name
@@ -148,7 +148,7 @@ class CloudmeshDatabase(object):
                 elif kind == "image":
                     images = provider.list_image(name)
 
-                    for image in images.values():
+                    for image in list(images.values()):
                         image['uuid'] = image['id']
                         image['type'] = 'string'
                         image['category'] = name
@@ -161,7 +161,7 @@ class CloudmeshDatabase(object):
 
                 elif kind == "vm":
                     vms = provider.list_vm(name)
-                    for vm in vms.values():
+                    for vm in list(vms.values()):
                         vm['uuid'] = vm['id']
                         vm['type'] = 'string'
                         vm['category'] = name
@@ -175,7 +175,7 @@ class CloudmeshDatabase(object):
                 elif kind == "secgroup":
                     secgroups = provider.list_secgroup(name)
                     # pprint(secgroups)
-                    for secgroup in secgroups.values():
+                    for secgroup in list(secgroups.values()):
                         secgroup_db_obj = self.db_obj_dict("secgroup",
                                                            name=secgroup['name'],
                                                            uuid=secgroup['id'],
@@ -216,7 +216,7 @@ class CloudmeshDatabase(object):
                 provider = BatchProvider(name)
 
                 vms = provider.list_job(name)
-                for vm in vms.values():
+                for vm in list(vms.values()):
                     vm['uuid'] = vm['id']
                     vm['type'] = 'string'
                     vm['category'] = name
@@ -521,7 +521,7 @@ class CloudmeshDatabase(object):
         # print("Inside add_obj")
         # print("Object Dict to add: {}".format(obj_dict))
 
-        for obj in obj_dict.values():
+        for obj in list(obj_dict.values()):
             # print(obj)
             for key in list(obj.keys()):
                 table_name = self.get_table(key)
