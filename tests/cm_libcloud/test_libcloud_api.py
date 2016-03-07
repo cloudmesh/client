50007a2850a19cde081e4f140c65e414216388b1
Gregor von Laszewski <laszewski@gmail.com>
2016-03-06 20:15:28 -0500

    new:dev: towards a libcloud ec2 test for the API

---
+++ tests/cm_libcloud/test_libcloud_api.py
@@ -0,0 +1,50 @@
+""" run with
+
+python setup.py install; nosetests -v --nocapture  tests/cm_libcloud/test_libcloud_api.py:Test_image.test_001
+
+nosetests -v --nocapture tests/libcloud/test_libcloud_api.py
+
+or
+
+nosetests -v tests/test_image.py
+
+"""
+
+from cloudmesh_client.common.Shell import Shell
+from cloudmesh_client.util import HEADING
+from cloudmesh_client.cloud.iaas.CloudProvider import CloudProvider
+import json
+
+
+
+class Test_libcloud_api():
+    """
+        This class tests the ImageCommand
+    """
+
+    def test_001(self):
+        """
+        test image refresh
+        :return:
+        """
+        HEADING()
+
+
+        from pprint import pprint
+
+        cloud = "chameleon-ec2"
+        provider = CloudProvider(cloud).provider
+
+        print (provider, type(provider))
+
+        # pprint (provider.__dict__)
+        # pprint (dir(provider))
+
+        r = provider.list_flavor(cloud)
+        pprint(r)
+
+        for kind in ["flavor", "image"]: # , "vm", "limits", "quota"]:
+            r = provider.list(kind, cloud)
+            pprint(r)
+
+        assert True
