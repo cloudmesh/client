1b295810664820e2aa433697e9f7a3f391ef4083
Gregor von Laszewski <laszewski@gmail.com>
2016-03-09 10:07:58 -0500

    new:dev: ec2 test example

--- tests/cm_libcloud/test_libcloud_api.py
+++ tests/cm_libcloud/test_libcloud_api.py
@@ -10,10 +10,10 @@ nosetests -v tests/test_image.py
 
 """
 
-from cloudmesh_client.common.Shell import Shell
 from cloudmesh_client.util import HEADING
 from cloudmesh_client.cloud.iaas.CloudProvider import CloudProvider
-import json
+from cloudmesh_client.common.ConfigDict import ConfigDict
+
 
 
 
@@ -23,8 +23,30 @@ class Test_libcloud_api():
     """
 
     def test_001(self):
+
+
+        from libcloud.compute.types import Provider
+        from libcloud.compute.providers import get_driver
+
+        config = ConfigDict('cloudmesh.yaml')
+        print (config)
+        credential = dict(config["cloudmesh"]["clouds"]["chameleon-ec2"]["credentials"])
+        print (credential)
+        EC2_ACCESS_KEY = credential['EC2_ACCESS_KEY']
+        EC2_SECRET_KEY = credential['EC2_SECRET_KEY']
+
+        Driver = get_driver(Provider.EC2)
+        conn = Driver(EC2_ACCESS_KEY, EC2_SECRET_KEY)
+
+        keys = conn.list_key_pairs()
+
+        print (keys)
+
+
+    '''
+    def test_002(self):
         """
-        test image refresh
+        test key list
         :return:
         """
         HEADING()
@@ -43,8 +65,9 @@ class Test_libcloud_api():
         #r = provider.list_flavor(cloud)
         #pprint(r)
 
-        for kind in ["image", "vm", "flavor", "key"]: # , "flavor", "vm", "limits", "quota"]:
+        for kind in ["key"]: # ["image", "vm", "flavor", "key"]: # , "flavor", "vm", "limits", "quota"]:
             r = provider.list(kind, cloud)
             pprint(r)
 
         assert True
+    '''
