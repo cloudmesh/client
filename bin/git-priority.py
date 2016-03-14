1f91d6c9ba3ee469a826cd46c2d5c3f4183df21d
Gregor von Laszewski <laszewski@gmail.com>
2016-03-14 09:02:57 -0400

    new:dev: git-priority code cleanup

--- bin/git-priority.py
+++ bin/git-priority.py
@@ -2,40 +2,61 @@
 from pprint import pprint
 import json
 import requests
+import sys
 
+columns = [
+    ("priority", "P", 4),
+    ("number", "N", 4),
+    ("title", "title", 40),
+    ("assignee", "assignee", 10),
+    ("milestone", "milestone", 10),
+    ("labels", "labels", 20)
+]
+
+#
+# setup format strings
+#
+header_format = ""
+row_format = ""
+header_elements = ['']
+row_elements = ['']
+headers = {}
+for (attribute, header, length) in columns:
+    headers[attribute] = header
+    header_elements.append("{}{}{}{}{}".format("{", header, ":", length, "}"))
+    row_elements.append("{}{}{}{}{}".format("{", attribute, ":", length, "}"))
+header_elements.append("")
+row_elements.append("")
+header_format = ' | '.join(header_elements).strip()
+row_format = ' | '.join(row_elements).strip()
+# print(row_format)
+# rint(header_format)
+
+
+#
+# Print header
+#
 issue = {}
-for (attribute, length) in \
-                [
-                ("P",4),
-                ("N",4),
-                ("title",40),
-                ("assignee",10),
-                ("milestone",10),
-                ("labels",20)
-                ]:
-    issue[attribute] = attribute
-print("| {P:4} | {N:4} | {title:40} | {assignee:10} | {milestone:10} | {labels:20} |".format(**issue))
+for (attribute, header, length) in columns:
+    issue[attribute] = header
+print(row_format.format(**issue))
+#
+# Print header line
+#
+issue = {}
+for (attribute, header, length) in columns:
+    issue[attribute] = "-" * length
+print(row_format.format(**issue))
 
-for (attribute, length) in \
-                [
-                ("P",4),
-                ("N",4),
-                ("title",40),
-                ("assignee",10),
-                ("milestone",10),
-                ("labels",20)
-                ]:
-    issue[attribute] = "-" * (length)
-print("| {P:4} | {N:4} | {title:40} | {assignee:10} | {milestone:10} | {labels:20} |".format(**issue))
+table = []
 
-
-for page in range(1,2):
+for page in range(1, 2):
     url = 'https://api.github.com/repos/cloudmesh/client/issues?page={}&per_page=100'.format(page)
     r = requests.get(url)
     issues = r.json()
+
     for issue in issues:
-        #print (issue)
-        #print (type(issue))
+        row = {}
         assignee = issue["assignee"]
         if issue["assignee"] is not None:
             issue["assignee"] = issue["assignee"]["login"]
@@ -55,7 +76,6 @@ for page in range(1,2):
         else:
             issue["labels"] = "None"
 
-
         priority = 999
         if issue["body"] is not None:
             body = issue["body"].splitlines()
@@ -68,17 +88,16 @@ for page in range(1,2):
         issue["priority"] = int(priority)
 
         content = ""
-        for (attribute, length) in \
-                [
-                ("priority",4),
-                ("number",4),
-                ("title",40),
-                ("assignee",10),
-                ("milestone",10),
-                ("labels",20)
-                ]:
-            data =  str(issue[attribute])
+        for (attribute, header, length) in columns:
+            data = str(issue[attribute])
             if len(data) > length:
-                issue[attribute] = data[:length-3] + "..."
-        print("| {priority:4} | {number:4} | {title:40} | {assignee:10} | {milestone:10} | {labels:20} |".format(**issue))
+                issue[attribute] = data[:length - 3] + "..."
+            row[attribute] = issue[attribute]
+        table.append(row)
 
+#
+# print the table
+#
+
+for row in table:
+    print(row_format.format(**row))
