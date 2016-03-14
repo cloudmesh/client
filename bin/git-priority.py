107ba1f5f89771a8dc2e0fb6078545c470e9ab9c
Gregor von Laszewski <laszewski@gmail.com>
2016-03-13 23:10:41 -0400

    new:dev: print the table in md format

--- bin/git-priority.py
+++ bin/git-priority.py
@@ -3,6 +3,31 @@ from pprint import pprint
 import json
 import requests
 
+issue = {}
+for (attribute, length) in \
+                [
+                ("P",4),
+                ("N",4),
+                ("title",20),
+                ("assignee",10),
+                ("milestone",10),
+                ("labels",20)
+                ]:
+    issue[attribute] = attribute
+print("| {P:4} | {N:4} | {title:20} | {assignee:10} | {milestone:10} | {labels:20} |".format(**issue))
+
+for (attribute, length) in \
+                [
+                ("P",4),
+                ("N",4),
+                ("title",20),
+                ("assignee",10),
+                ("milestone",10),
+                ("labels",20)
+                ]:
+    issue[attribute] = "-" * (length)
+print("| {P:4} | {N:4} | {title:20} | {assignee:10} | {milestone:10} | {labels:20} |".format(**issue))
+
 
 for page in range(1,2):
     url = 'https://api.github.com/repos/cloudmesh/client/issues?page={}&per_page=100'.format(page)
@@ -40,8 +65,20 @@ for page in range(1,2):
                 if "P:" in line:
                     priority = line.split("P:")[1]
 
-        issue["priority"] = priority
+        issue["priority"] = int(priority)
 
-        print ("| {priority} | {number} | {title} | {assignee} | {milestone} | {labels} |".format(**issue))
+        content = ""
+        for (attribute, length) in \
+                [
+                ("priority",4),
+                ("number",4),
+                ("title",20),
+                ("assignee",10),
+                ("milestone",10),
+                ("labels",20)
+                ]:
+            data =  str(issue[attribute])
+            if len(data) > length:
+                issue[attribute] = data[:length-3] + "..."
+        print("| {priority:4} | {number:4} | {title:20} | {assignee:10} | {milestone:10} | {labels:20} |".format(**issue))
 
-    #pprint (issues)
