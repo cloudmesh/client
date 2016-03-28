70e7a7f798a0185415e705d2bd79e8e0f4607fb9
Gregor von Laszewski <laszewski@gmail.com>
2016-03-28 16:13:41 -0400

    chg:dev: improve imports

--- cloudmesh_client/db/model.py
+++ cloudmesh_client/db/model.py
@@ -10,27 +10,6 @@ from sqlalchemy.ext.declarative import declared_attr
 from cloudmesh_client.common.ConfigDict import Config
 from cloudmesh_client.common.ConfigDict import ConfigDict
 
-from .base.model import COUNTER
-from .base.model import DEFAULT
-from .base.model import BATCHJOB
-from .base.model import LAUNCHER
-from .base.model import SECGROUP
-from .base.model import SECGROUPRULE
-from .base.model import VAR
-from .base.model import KEY
-from .base.model import GROUP
-from .base.model import RESERVATION
-
-
-from .openstack.model import IMAGE_OPENSTACK
-from .openstack.model import FLAVOR_OPENSTACK
-from .openstack.model import VM_OPENSTACK
-
-from .libcloud.model import IMAGE_LIBCLOUD
-from .libcloud.model import FLAVOR_LIBCLOUD
-from .libcloud.model import VM_LIBCLOUD
-
-
 """
 
 kind = the general type of the object that can be helpful for the location
