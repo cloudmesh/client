50eeeb5d0ec765a0996f83f31f9a1cefa982040e
Gregor von Laszewski <laszewski@gmail.com>
2016-04-21 12:33:22 -0400

    new:dev: implemented add and list function for launcher

--- cloudmesh_client/shell/plugins/LauncherCommand.py
+++ cloudmesh_client/shell/plugins/LauncherCommand.py
@@ -8,9 +8,10 @@ from cloudmesh_client.default import Default
 from cloudmesh_client.shell.command import command, PluginCommand, \
     CloudPluginCommand, CometPluginCommand
 from cloudmesh_client.shell.console import Console
+from cloudmesh_client.common.hostlist import Parameter
 
 class LauncherCommand(PluginCommand, CloudPluginCommand, CometPluginCommand):
-    topics = {"launcher": "cloud"}
+    topics = {"launcher": "todo"}
 
     def __init__(self, context):
         self.context = context
@@ -24,9 +25,9 @@ class LauncherCommand(PluginCommand, CloudPluginCommand, CometPluginCommand):
         ::
 
           Usage:
-              launcher list [NAME] [--cloud=CLOUD] [--format=FORMAT] [--all]
+              launcher list [NAMES] [--cloud=CLOUD] [--format=FORMAT]
               launcher add NAME SOURCE
-              launcher delete [NAME] [--cloud=CLOUD]
+              launcher delete [NAMES] [--cloud=CLOUD]
               launcher clear
               launcher run [NAME]
               launcher resume [NAME]
@@ -65,47 +66,61 @@ class LauncherCommand(PluginCommand, CloudPluginCommand, CometPluginCommand):
             launcher list --cloud=general
             launcher delete <KEY>
         """
-        pprint(arguments)
 
-        data = dotdict(arguments)
-        cloud = arguments["--cloud"] or Default.cloud
-        name = data.NAME
-        # data.cloud = arguments["--cloud"] or Default.cloud
-        # c = data.list
-        # print ("CCCC", c, data.NAME, data.cloud)
-        # print ("Hallo {cloud} is list={list}".format(**data))
+        arg = dotdict(arguments)
+        arg.cloud = arguments["--cloud"] or Default.cloud
+        if arg.NAMES is not None:
+            arg.names = Parameter.expand(arg.NAMES)
+        else:
+            arg.names = None
+        if arg.name == ['all']:
+            arg.names = None
+        arg.output = arguments['--format'] or 'table'
+        pprint (arg)
+        
+        # arg.cloud = arguments["--cloud"] or Default.cloud
+        # c = arg.list
+        # print ("CCCC", c, arg.NAME, arg.cloud)
+        # print ("Hallo {cloud} is list={list}".format(**arg))
         # launcher = Launcher(kind=None)
 
-        if cloud is None:
-            Console.error("Default cloud not set")
+        if arg.cloud is None:
+            Console.error("Default arg.cloud not set")
             return
 
         result = ""
 
-        if arguments["delete"]:
-            if name is not None:
-                result = Launcher.delete(name=name, category=cloud)
+        if arguments["list"]:
+
+            arg.format = arguments.get("--format", "table")
+
+            result = Launcher.list(name=arg.names, output=arg.output)
+
+        elif arg.add:
+
+            result = Launcher.add(name=arg.NAME, source=arg.SOURCE)
+
+        elif arguments["delete"]:
+            if arg.name is not None:
+                result = Launcher.delete(name=arg.name, category=arg.cloud)
             else:
                 result = Launcher.delete(name=None)
 
         elif arguments["run"]:
             result = Launcher.run()
 
-        elif arguments["list"]:
-            data.format = arguments.get("--format", "table")
-            result = Launcher.list(name=name, output=data.format, category=cloud)
 
         elif arguments["resume"]:
-            result = Launcher.resume(name=name)
+            result = Launcher.resume(name=arg.name)
 
         elif arguments["suspend"]:
-            result = Launcher.suspend(name=name)
+            result = Launcher.suspend(name=arg.name)
 
         elif arguments["details"]:
-            result = Launcher.details(name=name)
+            result = Launcher.details(name=arg.name)
 
         elif arguments["clear"]:
-            result = Launcher.clear(name=name)
+            result = Launcher.clear(name=arg.name)
 
         elif arguments["refresh"]:
             result = Launcher.refresh(name=name)
