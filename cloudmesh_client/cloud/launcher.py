644941e4e8aacfe3cf367c6b89747fab86aa6953
Gregor von Laszewski <laszewski@gmail.com>
2016-04-21 12:33:22 -0400

    new:dev: implemented add and list function for launcher

--- cloudmesh_client/cloud/launcher.py
+++ cloudmesh_client/cloud/launcher.py
@@ -1,48 +1,92 @@
 from __future__ import print_function
 import subprocess
 
-from cloudmesh_client.cloud.ListResource import ListResource
 from cloudmesh_client.shell.console import Console
+from cloudmesh_client.common.Printer import Printer
+from cloudmesh_client.db import CloudmeshDatabase
+from cloudmesh_client.cloud.iaas.CloudProvider import CloudProvider
+from cloudmesh_client.cloud.ListResource import ListResource
 
 # noinspection PyUnusedLocal
-class LauncherProvider(ListResource):
+class Launcher(ListResource):
+
+    cm = CloudmeshDatabase()
+
     def info(self, **kwargs):
         Console.TBD("not yet implemented")
         return "not yet implemented"
 
-    def list(self, **kwargs):
+    @classmethod
+    def list(cls, name=None, output='table'):
+
+        # ignore names for now
+
+        try:
+
+            elements = cls.cm.find(kind="launcher", category='general', scope="all", output="dict")
+
+            order = None
+            header = None
+
+            return Printer.write(elements,
+                                 order=order,
+                                 header=header,
+                                 output=output)
+        except Exception as ex:
+            Console.error(ex.message)
+            return ""
+
+    @classmethod
+    def add(cls, name=None, source=None):
+
+        d = {
+            "kind": "launcher",
+            "provider": "general",
+            "category": "general",
+            "name": name,
+            "source": source,
+            "parameter": "We find that in source"
+        }
+
+        cls.cm.add(d)
+        return "not yet implemented"
+
+    @classmethod
+    def run(cls, **kwargs):
         Console.TBD("not yet implemented")
         return "not yet implemented"
 
-    def run(self, **kwargs):
+    @classmethod
+    def resume(cls, **kwargs):
         Console.TBD("not yet implemented")
         return "not yet implemented"
 
-    def resume(self, **kwargs):
+    @classmethod
+    def suspend(cls, **kwargs):
         Console.TBD("not yet implemented")
         return "not yet implemented"
 
-    def suspend(self, **kwargs):
+    @classmethod
+    def kill(cls, **kwargs):
         Console.TBD("not yet implemented")
         return "not yet implemented"
 
-    def kill(self, **kwargs):
+    @classmethod
+    def details(cls, **kwargs):
         Console.TBD("not yet implemented")
         return "not yet implemented"
 
-    def details(self, **kwargs):
+    @classmethod
+    def clear(cls, **kwargs):
         Console.TBD("not yet implemented")
         return "not yet implemented"
 
-    def clear(self, **kwargs):
+    @classmethod
+    def refresh(cls, **kwargs):
         Console.TBD("not yet implemented")
         return "not yet implemented"
 
-    def refresh(self, **kwargs):
-        Console.TBD("not yet implemented")
-        return "not yet implemented"
-
-
+"""
 class LauncherShell(LauncherProvider):
     def __init__(self):
         pass
@@ -56,8 +100,9 @@ class LauncherShell(LauncherProvider):
         print("OOOOOO", output)
 
         return output
+"""
 
-
+"""
 # noinspection PyPep8Naming
 def Launcher(kind):
 
@@ -67,3 +112,4 @@ def Launcher(kind):
     #else:
         Console.TBD("not yet implemented")
         return "not yet implemented"
+"""
