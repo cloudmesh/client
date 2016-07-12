65dc8e2fd4c5c9d13c49b6421a0d5c20798658c6
Gregor von Laszewski <laszewski@gmail.com>
2016-07-12 13:22:15 -0400

    new:dev: deployment script for xenial

---
+++ bin/deploy-on-xenial-vm.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+sudo apt install python-pip -y
+sudo apt install libssl-dev -y
+sudo pip install pip -U
+sudo apt install git -y
+sudo pip install ansible
+sudo pip install cloudmesh_client
+python --version
+pip --version
+git –version
