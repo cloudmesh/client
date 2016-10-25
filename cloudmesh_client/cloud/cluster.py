7c56afab9f56cd3233c468dbc653ee7f0bbea6bb
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-10-25 12:20:07 -0400

    new:dev: add new 'ClusterModel' class sitting between DB and Cluster
    DB <-> ClusterModel <-> Cluster

--- cloudmesh_client/cloud/cluster.py
+++ cloudmesh_client/cloud/cluster.py
@@ -1,12 +1,89 @@
 from __future__ import print_function
 
-from cloudmesh_client.db import CloudmeshDatabase
+from sqlalchemy import Column, Boolean, String
+
+from cloudmesh_client.common.dotdict import dotdict
+from cloudmesh_client.db import CloudmeshDatabase, CloudmeshMixin
 from cloudmesh_client.cloud.vm import Vm
 
 from cloudmesh_client.cloud.network import Network
 
 
 _db = CloudmeshDatabase()
+
+
+class ClusterModel(CloudmeshMixin, CloudmeshDatabase.Base):
+    __tablename__ = 'cluster'
+    __kind__ = 'cluster'
+
+    name = Column(String)
+    cloudname = Column(String)
+    username = Column(String)
+    imagename = Column(String)
+    flavorname = Column(String)
+    keyname = Column(String)
+    secgroupname = Column(String)
+    assignFloatingIP = Column(Boolean)
+
+    @classmethod
+    def _cluster(cls, **kwargs):
+        """Create a single :class:`Cluster` instance from a single row in the
+        cluster table
+
+        Here, ``kwargs`` is a :class:`dict` mapping column name to
+        value for this row.
+
+        :returns: the cluster
+        :rtype: :class:`Cluster`
+        """
+
+        cluster_details = dotdict(kwargs)
+        table_name = 'vm_{}'.format(cluster_details.provider)
+        table = _db.table(name=table_name)
+
+        cluster = Cluster(
+            name=cluster_details.name,
+            cloudname=cluster_details.cloud,
+            username=cluster_details.user,
+            imagename=cluster_details.image,
+            secgroupname=cluster_details.secgroup,
+            assignFloatingIP=cluster_details.assignFloatingIP
+        )
+
+        for vm_details in _db.find(table=table,
+                                   cluster=cluster_details.cluster):
+            cluster.add_existing_instance(vm_details)
+
+        return cluster
+
+    @classmethod
+    def iter(cls):
+        """List all the entries
+
+        :returns: all the Clusters
+        :rtype: :class:`list` of :class:`Cluster`
+        """
+
+        for key_value in _db.find(table=cls):
+            cluster = cls._cluster(**key_value)
+            yield cluster
+
+
+    @classmethod
+    def register(cls, cluster):
+        """Register a cluster to the database
+
+        :param Cluster cluster: the cluster to register
+        :returns: 
+        :rtype: 
+        """
+
+        row_values = cluster.__dict__
+        row_values.pop('_instances')
+        obj = cls(**row_values)
+
+        _db.insert(obj)
+
 
 
 class Cluster(object):
@@ -31,17 +108,6 @@ class Cluster(object):
 
         self._instances = list()
 
-    @classmethod
-    def create(cls, **kwargs):
-        """Either load a cluster or create a new one.
-
-        kwargs are passed directly to the constructor
-        """
-
-        # FIXME: check database
-        return cls(**kwargs)
-
-
     def __len__(self):
         return len(self._instances)
 
@@ -61,7 +127,6 @@ class Cluster(object):
         Vm.refresh(cloud=self.cloudname)
 
         return ip
-
 
     def add_instance(self):
         """Boots a new instance and adds it to this cluster
@@ -83,5 +148,13 @@ class Cluster(object):
         if self.assignFloatingIP:
             vm['floating_ip'] = self._assign_floating_ip(vm)
 
-        self._instances.append(vm)
+        self.add_existing_instance(vm)
 
+    def add_existing_instance(self, vm_details):
+        """Adds a previously booted instance to the cluster.
+
+        :param vm_details: a dict-like object containing the 
+                           details of the vm (eg from Vm.boot)
+        """
+
+        self._instances.append(vm_details)
