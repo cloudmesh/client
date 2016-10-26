ecef68468ef77f7f78883dc78dd322a9a2764676
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-10-26 14:46:47 -0400

    new:dev: add module for custom exceptions

---
+++ cloudmesh_client/exc.py
@@ -0,0 +1,24 @@
+"""
+This module contains the exceptions
+"""
+
+
+
+class UnrecoverableErrorException(Exception):
+    """Occurs on an error that cannot be recovered automatically.
+    Requires user interaction to change the state.
+    """
+
+    pass
+
+
+class ClusterNameClashException(Exception):
+    """Occurs when a cluster is created with a preexisting name
+    """
+
+    def __init__(self, tablename, name):
+        self.tablename = tablename
+        self.name = name
+
+    def __str__(self):
+        return 'Cluster {} already exists'.format(self.name)
