33abd72ef1d3106c54fdf3108b09e7997490bdbc
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-12-01 13:15:56 -0500

    chg:dev: major !refactor of ansible playbook

--- cloudmesh_client/deployer/ansible/playbook.py
+++ cloudmesh_client/deployer/ansible/playbook.py
@@ -1,61 +1,89 @@
 import json
 import operator
+import tempfile
+
+import yaml
 
 from ansible.inventory import Inventory
 from ansible.parsing.dataloader import DataLoader
 from ansible.playbook import Playbook
 from ansible.vars import VariableManager
 
-from cloudmesh_client.common.Shell import Subprocess, SubprocessError
+from cloudmesh_client.shell.console import Console
+from cloudmesh_client.common.Shell import Subprocess
 
 
-class AnsiblePlaybookRunner(object):
+class AnsiblePlaybook(object):
 
-    def __init__(self, inventory, path, user=None, become=False,
+    def __init__(self, inventory, roles=None, path=None, username=None, become=False,
                  become_user=None, forks=None, modifyKnownHosts=True,
-                 extra_vars=None):
+                 extra_vars=None, subprocess_kwargs=None):
         self._inventory = inventory
+        self._roles = roles or []
         self._path = path
-        self._user = user
+        self._username = username
         self._become = become
         self._become_user = become_user
         self._forks = forks
         self._modifyKnownHosts = modifyKnownHosts
         self._extra_vars = extra_vars
+        self._subprocess_kwargs = subprocess_kwargs or dict()
 
-        self._subprocess_kwargs = dict()
+        ################################################################ rest of setup
 
-    def set_cwd(self, path):
-        self._subprocess_kwargs['cwd'] = path
-        return self
+        self._inventory_file = tempfile.NamedTemporaryFile()
+        self._playbook_file  = tempfile.NamedTemporaryFile()
 
-    def set_stderr(self, stream):
-        self._subprocess_kwargs['stderr'] = stream
-        return self
+        # write inventory
+        self._inventory_file.write(self._inventory.ini())
+        self._inventory_file.flush()
 
-    def set_stdout(self, stream):
-        self._subprocess_kwargs['stdout'] = stream
-        return self
+        Console.debug_msg('Inventory:\n' + self._inventory.ini())
 
-    def set_env(self, environment):
-        self._subprocess_kwargs['env'] = environment
-        return self
+        # write playbook
+        playbook = [{
+            'name': role.name,
+            'hosts': role.hosts,
+            'become': role.become,
+            'roles': [dict(role=role.path,
+                           **role.variables)]}
+                    for role in roles]
+        yml = yaml.dump(playbook)
+        self._playbook_file.write(yml)
+        self._playbook_file.flush()
 
-    def set_secure(self, secure_p):
-        self._modifyKnownHosts = secure_p
-        return self
+        Console.debug_msg('Playbook:\n' + yml)
+
+    def run(self):
+        return self()
 
     def __call__(self):
+
         cmd_builder_monoid = [
             ['ansible-playbook'],
-            ['--inventory', self._inventory] if self._inventory else [],
-            ['--user', self._user] if self._user else [],
-            ['--become'] if self._become else [],
-            ['--become-user', self._become_user] if self._become_user else [],
-            ['--forks', str(self._forks)] if self._forks else [],
-            ['--ssh-extra-args', '-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'] if not self._modifyKnownHosts else []
-            ['--extra-vars', json.dumps(self._extra_vars)] if self._extra_vars else [],
-            [self._path]
+
+            ['--inventory', self._inventory_file.name]
+            if self._inventory else [],
+
+            ['--user', self._username]
+            if self._username else [],
+
+            ['--become']
+            if self._become else [],
+
+            ['--become-user', self._become_user]
+            if self._become_user else [],
+
+            ['--forks', str(self._forks)]
+            if self._forks else [],
+
+            ['--ssh-extra-args', '-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no']
+            if not self._modifyKnownHosts else []
+
+            ['--extra-vars', json.dumps(self._extra_vars)]
+            if self._extra_vars else [],
+
+            [self._playbook_file.name],
         ]
 
         cmd = reduce(operator.concat, cmd_builder_monoid)
