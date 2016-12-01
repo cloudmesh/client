e624246d5565d8eef600b87939a984f7ebd13800
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-12-01 13:15:38 -0500

    chg:dev: major !refactor of ansible role

--- cloudmesh_client/deployer/ansible/role.py
+++ cloudmesh_client/deployer/ansible/role.py
@@ -1,53 +1,64 @@
 
 
 import os.path
-import shutil
-import tempfile
+# import shutil
+# import tempfile
 
-import yaml
-
-from cloudmesh_client.deployer.ansible.playbook import AnsiblePlaybookRunner
+# import yaml
 
 
 class AnsibleRole(object):
 
-    def __init__(self, path, become=True, variables=None):
-        assert path is not None
+    def __init__(self, uri, hosts='all', become=True, variables=None, group='all'):
+        assert uri is not None
 
-        self.path = path
+        self.uri = uri
+        self.path = self._materialize_uri(self.uri)
+
         self.name = os.path.basename(self.path)
+        self.hosts = hosts
         self.directory = os.path.dirname(self.path)
         self.become = become
         self.variables = variables or {}
+        self.group = group
 
 
-    def run(self, inventory, user=None):
-        tempdir = tempfile.mkdtemp()
+    def _materialize_uri(self, uri):
+        if os.path.exists(uri):
+            return uri
+        elif uri.startswith('file://'):
+            return uri.lstrip('file://')
+        else:
+            raise NotImplementedError(uri)
 
-        playbook = [{
-            'name': 'Cloudmesh Role: ' + self.path,
-            'hosts': 'all',
-            'become': True if self.become else False,
-            'roles': [dict(role=self.path,
-                           **self.variables)]
-        }]
 
-        y = yaml.dump(playbook, default_flow_style=False)
-        play = os.path.join(tempdir, 'playbook.yml')
-        with open(play, 'w') as fd:
-            fd.write(y)
+    # def run(self, inventory, user=None):
+    #     tempdir = tempfile.mkdtemp()
 
-        inventory_file = os.path.join(tempdir, 'inventory.txt')
-        with open(inventory_file, 'w') as fd:
-            fd.write(inventory)
+    #     playbook = [{
+    #         'name': 'Cloudmesh Role: ' + self.path,
+    #         'hosts': 'all',
+    #         'become': True if self.become else False,
+    #         'roles': [dict(role=self.path,
+    #                        **self.variables)]
+    #     }]
 
-        runner = AnsiblePlaybookRunner(inventory_file, play, user=user, modifyKnownHosts=False)
-        runner.set_cwd(tempdir)
+    #     y = yaml.dump(playbook, default_flow_style=False)
+    #     play = os.path.join(tempdir, 'playbook.yml')
+    #     with open(play, 'w') as fd:
+    #         fd.write(y)
 
-        # don't hide ansible's stderr/stdout
-        runner.set_stderr(None)
-        runner.set_stdout(None)
+    #     inventory_file = os.path.join(tempdir, 'inventory.txt')
+    #     with open(inventory_file, 'w') as fd:
+    #         fd.write(inventory)
 
-        runner()
+    #     runner = AnsiblePlaybookRunner(inventory_file, play, user=user, modifyKnownHosts=False)
+    #     runner.set_cwd(tempdir)
 
-        shutil.rmtree(tempdir)
+    #     # don't hide ansible's stderr/stdout
+    #     runner.set_stderr(None)
+    #     runner.set_stdout(None)
+
+    #     runner()
+
+    #     shutil.rmtree(tempdir)
