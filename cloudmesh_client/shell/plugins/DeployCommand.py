a0b8dde342db77a535829a7367dc6ad10d39ade5
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-12-01 13:18:30 -0500

    chg:dev: major !refactor of !cmd deploy to reflect ansible changes

--- cloudmesh_client/shell/plugins/DeployCommand.py
+++ cloudmesh_client/shell/plugins/DeployCommand.py
@@ -1,14 +1,15 @@
 
 import os.path
 
+from cloudmesh_client.platform.virtual_cluster.cluster import Cluster
 from cloudmesh_client.common.dotdict import dotdict
 from cloudmesh_client.default import Default
+from cloudmesh_client.deployer.ansible.inventory import Inventory
 from cloudmesh_client.deployer.ansible.role import AnsibleRole
+from cloudmesh_client.deployer.ansible.playbook import AnsiblePlaybook
 from cloudmesh_client.shell.command import (CloudPluginCommand, PluginCommand,
     command)
 from cloudmesh_client.shell.console import Console
-from cloudmesh_client.shell.plugins.ClusterCommand2 import (
-    Command as ClusterCommand)
 
 
 class AnsibleCommand(object):
@@ -17,20 +18,31 @@ class AnsibleCommand(object):
         Console.debug_msg('NotImplementedError')
 
 
-    def role(self, cluster=None, path=None, user=None):
+    def role(self, cluster=None, uris=None, hostsPattern='all',
+             become=True, username=None, modifyKnownHosts=True):
         """Deploy a role to all the nodes in the cluster
 
         :param str cluster: cluster name (default is the active cluster)
         :param str path: path to the role (default is current working directory)
-        :param str name: name of the role
-
+        :param str username: login username of the nodes (overrides autodetected value)
+        :param modifyKnownHosts: whether to update ~/.ssh/known_hosts
         """
 
-        cluster = cluster or Default.active_cluster
+        uris = uris or []
+        assert len(uris) > 0, uris
 
-        inventory = ClusterCommand().inventory(cluster=cluster, format='ansible').ini()
-        role = AnsibleRole(path)
-        role.run(inventory, user=user)
+        cluster = Cluster.from_name(cluster or Default.cluster)
+        inventory = Inventory.from_cluster(cluster)
+        roles = [AnsibleRole(uri, become=become, hosts=hostsPattern) for uri in uris]
+        play = AnsiblePlaybook(
+            roles=roles,
+            inventory=inventory,
+            username=username,
+            modifyKnownHosts=modifyKnownHosts,
+            subprocess_kwargs=dict(stdout=None, stderr=None),
+        )
+
+        play.run()
 
 
 
@@ -45,7 +57,7 @@ class DeployCommand(PluginCommand, CloudPluginCommand):
         """
         ::
             Usage:
-              deploy ansible [-p PATH...] [-u NAME] [CLUSTER]
+              deploy ansible role [-c CLUSTER] [-u NAME] [-N] [-H HOST] [-b] URI...
 
             Commands:
 
@@ -60,43 +72,41 @@ class DeployCommand(PluginCommand, CloudPluginCommand):
               CLUSTER                        Cluster name to deploy to
               NAME                           Alphanumeric name
               COUNT                          Integer > 0
-              PATH                           Path to entry on the filesystem
+              URI                            Location of the item as a uri
+              HOST                           Host matching pattern
 
             Options:
 
-              -p --path=PATH                 Path to the location of the item
-              -u --user=NAME                 Username of the nodes to manage
-
+              -c --cluster=CLUSTER           Cluster name to operate on (defaults to active)
+              -N --no-modify-known-hosts     Don't let ssh update ~/.ssh/known_hosts
+              -u --username=NAME             Username of the nodes to manage
+              -H HOST --hosts=HOST           Host matching pattern [default: all]
+              -b --no-become                 Don't become privileged user
         """
 
         arguments = dotdict(arguments)
         from pprint import pprint
         pprint(arguments)
 
-        if arguments.ansible:
+        if arguments.ansible and arguments.role:
             ansible = AnsibleCommand()
-            for path in arguments['--path']:
-
-                # playbook
-                if os.path.isfile(path):
-                    ansible.playbook(
-                        cluster = arguments.CLUSTER,
-                        path = path,
-                        user = arguments['--user'],
-                    )
-
-                # role
-                elif os.path.isdir(path):
-                    ansible.role(
-                        cluster = arguments.CLUSTER,
-                        path = path,
-                        user = arguments['--user'],
-                    )
+            ansible.role(
+                cluster = arguments.CLUSTER,
+                hostsPattern = arguments['--hosts'],
+                uris = arguments.URI,
+                become = not arguments['--no-become'],
+                username = arguments['--username'],
+                modifyKnownHosts = not arguments['--no-modify-known-hosts'],
+            )
 
 
 if __name__ == '__main__':
-    import sys
-    rolepath = sys.argv[1]
+    from docopt import docopt
+    print(docopt(DeployCommand.do_deploy.__doc__))
 
-    c = AnsibleCommand()
-    c.role(path=rolepath)
+
+    # import sys
+    # rolepath = sys.argv[1]
+
+    # c = AnsibleCommand()
+    # c.role(path=rolepath)
